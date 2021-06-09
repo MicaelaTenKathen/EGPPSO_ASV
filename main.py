a9c50c881025054a7ec9c827e9e8aa7a93bd16c3
MicaelaTenKathen <micaela.tenkathen@gmail.com>
2021-06-09 09:44:21 +0200

    Initial commit

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ from Enviroment.plots import *
 
 import time
 
-
+# The size of the map is determined
 xs, ys = 100, 150
 
+
+# The variable num correspond to the ground truth used
 num = 9
 
 if num == 0:
@@ -58,19 +60,27 @@ elif num == 10:
     bench_function, X_test, grid, df_bounds, grid_or = bench_total(path[-1] + '/Data/shww10.npy'.format(0), xs, ys,
                                                                    load_file=False, load_from_db=False)
 
+
 grid_min, grid_max, grid_max_x, grid_max_y = map_values(xs, ys)
 
+
+# The number of iterations is selected
 GEN = 6000
 
-seed = [20, 95, 541, 65, 145, 156, 158, 12, 3, 89, 57, 123, 456, 789, 987, 654, 321, 147, 258, 369, 741, 852, 963, 159,
-        951, 753, 357, 756, 8462, 4875]
+# Seed corresponds to the different initial configurations
+seed = [20] #, 95, 541, 65, 145, 156, 158, 12, 3, 89, 57, 123, 456, 789, 987, 654, 321, 147, 258, 369, 741, 852, 963, 159,
+        #951, 753, 357, 756, 8462, 4875]
+
 
 # for i in range(len(seed)):
 gp_best, mu_best = [0, 0], [0, 0]
 
+# At the beginning, the acceleration coefficient c3 and c4 are 0
 c3, c4 = 0, 0
 
+# Values obtained from hyper-parameter optimization
 c1, c2, c31, c41, length_scale, lam = 3.1286, 2.568, 0.79, 0, 1, 0.1
+
 
 part_dist, part_ant, distances = np.zeros(8), np.zeros((GEN + 1, 8)), np.zeros(4)
 n_data, n_plot = 1, 1
@@ -80,28 +90,34 @@ x_p, y_p, y_data, part_data, x_g, y_g, y_mult = list(), list(), list(), list(), 
 fitness, x_h, y_h = list(), list(), list()
 ok = False
 
+# Name of different excel documents
 e1, e2, e3, e4, e5 = 'Pruebas/Error' + str(seed[0]) + '.xlsx', 'Pruebas/Sigma' + str(seed[0]) + '.xlsx', \
                      'Pruebas/Mu' + str(seed[0]) + '.xlsx', 'Pruebas/Distance' + str(seed[0]) + '.xlsx', \
                      'Pruebas/Data' + str(seed[0]) + '.xlsx'
 
+# PSO initialization
 initPSO()
 generate(grid_min, grid_max)
 toolbox = tool_n(grid_min, grid_max, generate, updateParticle_n)
-random.seed(20)
+random.seed(seed[0])
 pop, best = swarm(toolbox, 4)
 stats, logbook = statistic()
+
 
 part_array = list()
 s_ant = np.zeros(4)
 s_n = np.array([True, True, True, True])
 
-ker = RBF(length_scale=length_scale, length_scale_bounds=(1e-1, 10))
-post_array = [length_scale, length_scale, length_scale, length_scale]
 
+# GP configuration
+ker = RBF(length_scale=length_scale, length_scale_bounds=(1e-1, 10))
 gpr = GaussianProcessRegressor(kernel=ker, alpha=1 ** 2)  # alpha = noise**2
 
+
+post_array = [length_scale, length_scale, length_scale, length_scale]
 start_time = time.time()
 
+# First iteration of the PSO
 for part in pop:
     print(part)
     ok, x_h, y_h, fitness, x_p, y_p, y_data, x_bench, y_bench, part, best, n_plot, s_n = part_fitness(grid, ok, x_h,
@@ -124,11 +140,13 @@ for part in pop:
     if n_data > 4:
         n_data = 1
 
+# Speed and Position update
 for part in pop:
     toolbox.update(part, best, gp_best, mu_best, g, GEN, c1, c2, c3, c4)
 
 ok = False
 
+# Main part of the code
 for g in range(GEN):
     for part in pop:
 
@@ -192,6 +210,7 @@ for g in range(GEN):
                                                                                                               file=True,
                                                                                                               init=False)
 
+            # GP update
             sigma, mu, x_a, y_a, post_array = gp_regression(n_data, x_h, y_h, fitness, X_test, gpr, post_array)
             sigma_data, mu_data = gpr_value(g, int(part[0]), int(part[1]), X_test, sigma, mu, sigma_data, mu_data)
 
@@ -218,7 +237,7 @@ data = {'Seed': seed, 'GEN': GEN, 'Time': time.time() - start_time, 'MSE_GEN': M
         'Avr_dist': np.mean(distances)}
 
 
-# savexlsx(MSE_data, sigma_data, mu_data, distances, it, e1, e2, e3, e4, e5)
+savexlsx(MSE_data, sigma_data, mu_data, distances, it, e1, e2, e3, e4, e5)
 plot_gaussian(ys, x_g, y_g, n, mu, sigma, X_test, grid_or, grid_min, part_ant)
 plot = plot_benchmark(xs, ys, grid_or, bench_function, X_test)
 plot_error(MSE_data, it, GEN)
